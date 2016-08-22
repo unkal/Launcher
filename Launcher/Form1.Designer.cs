af6d6abfc0e96fd9f95254304efdd15786d57fb1
unkal <unkal@yandex.ru>
2016-08-22 13:00:02 +0300

    Launcher
    firstProjectDiplom

---
+++ Launcher/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Launcher
+{
+    partial class Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Launcher));
+            this.go = new System.Windows.Forms.Button();
+            this.game = new System.Windows.Forms.Button();
+            this.password = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.registration = new System.Windows.Forms.Button();
+            this.ConnectUser = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // go
+            // 
+            this.go.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.go.Location = new System.Drawing.Point(303, 35);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(105, 26);
+            this.go.TabIndex = 2;
+            this.go.Text = "Войти";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // game
+            // 
+            this.game.BackColor = System.Drawing.Color.Transparent;
+            this.game.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.game.Enabled = false;
+            this.game.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.game.Location = new System.Drawing.Point(454, 35);
+            this.game.Name = "game";
+            this.game.Size = new System.Drawing.Size(116, 58);
+            this.game.TabIndex = 4;
+            this.game.Text = "ИГРАТЬ";
+            this.game.UseVisualStyleBackColor = false;
+            this.game.Click += new System.EventHandler(this.game_Click);
+            // 
+            // password
+            // 
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.password.Location = new System.Drawing.Point(185, 37);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(100, 17);
+            this.password.TabIndex = 1;
+            // 
+            // login
+            // 
+            this.login.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.login.Location = new System.Drawing.Point(60, 37);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 17);
+            this.login.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(57, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Логин:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(182, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Пароль:";
+            // 
+            // registration
+            // 
+            this.registration.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.registration.Location = new System.Drawing.Point(303, 67);
+            this.registration.Name = "registration";
+            this.registration.Size = new System.Drawing.Size(105, 26);
+            this.registration.TabIndex = 3;
+            this.registration.Text = "Регистрация";
+            this.registration.UseVisualStyleBackColor = true;
+            this.registration.Click += new System.EventHandler(this.registration_Click);
+            // 
+            // ConnectUser
+            // 
+            this.ConnectUser.AutoSize = true;
+            this.ConnectUser.BackColor = System.Drawing.Color.Transparent;
+            this.ConnectUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ConnectUser.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.ConnectUser.Location = new System.Drawing.Point(10, 27);
+            this.ConnectUser.Name = "ConnectUser";
+            this.ConnectUser.Size = new System.Drawing.Size(169, 20);
+            this.ConnectUser.TabIndex = 8;
+            this.ConnectUser.Text = "Добро пожаловать";
+            this.ConnectUser.Visible = false;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.BackColor = System.Drawing.Color.Transparent;
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(57, 57);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 13);
+            this.labelError.TabIndex = 9;
+            // 
+            // Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Launcher.Properties.Resources._111;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(634, 398);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.ConnectUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.registration);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.game);
+            this.Controls.Add(this.login);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Launcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Launcher_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.Button game;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button registration;
+        private System.Windows.Forms.Label ConnectUser;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ Launcher/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Launcher
+{
+    public partial class Launcher : Form
+    {
+        static private Socket send;
+        Thread th;
+        private IPAddress ip = IPAddress.Parse("192.168.0.84");
+        private int port = 1234;
+        string msgserv;
+        public Launcher()
+        {
+            InitializeComponent();          
+        }
+     
+        //connect to server
+        private void go_Click(object sender, EventArgs e)
+        {
+            labelError.Text = "";
+            if (go.Text == "Войти")
+            {
+                th = new Thread(delegate ()
+                {
+                    IPEndPoint ipEndPoint = new IPEndPoint(ip, port);
+                    send = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    try
+                    {
+                    // Соединяем сокет с удаленной точкой
+                    send.Connect(ipEndPoint);
+                    string message = "1000" + login.Text + "@" + password.Text;
+                    // message = "ХУЙПИЗДАДЖИГУРДА!!!!";
+                    byte[] msg = Encoding.UTF8.GetBytes(message);
+                    // Отправляем данные через сокет
+                    send.Send(msg);
+
+                    // Получаем ответ от сервера
+                    byte[] bytes = new byte[20];
+                    send.Receive(bytes);
+                    msgserv = Encoding.UTF8.GetString(bytes);
+
+                        switch (msgserv.Substring(0, 4))
+                        {
+                            case "1001":
+                                MessageBox.Show(msgserv);
+                                this.Invoke((MethodInvoker)delegate ()
+                                {
+                                    game.Enabled = true;
+                                    registration.Visible = false;
+                                    go.Text = "Выйти";
+                                    login.Visible = false;
+                                    password.Visible = false;
+                                    label1.Visible = false;
+                                    label2.Visible = false;
+                                    ConnectUser.Text = "Добро пожаловать :" + login.Text;
+                                    ConnectUser.Visible = true;
+                                });
+                                break;
+                            case "1000":
+                                this.Invoke((MethodInvoker)delegate ()
+                                {
+                                    labelError.Text = "Неверный логин или пароль";
+                                });
+                                break;
+                        }
+                    // Освобождаем сокет
+                    send.Shutdown(SocketShutdown.Both);
+                        send.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        if (Application.OpenForms["Launcher"] != null)// form is runing
+                        {
+                            MessageBox.Show(ex.ToString());
+                            this.Invoke((MethodInvoker)delegate ()
+                            {
+                                game.Enabled = false;
+                                registration.Visible = true;
+                                go.Text = "Войти";
+                                login.Visible = true;
+                                password.Visible = true;
+                                label1.Visible = true;
+                                label2.Visible = true;
+                                ConnectUser.Visible = false;
+                            });
+                        }
+                        else
+                        {
+                            System.Diagnostics.Process.GetCurrentProcess().Kill();
+                        }
+
+                    }
+                });
+                th.Start();
+            }
+            else if (go.Text=="Выйти")
+                {
+                game.Enabled = false;
+                registration.Visible = true;
+                go.Text = "Войти";
+                login.Visible = true;
+                password.Visible = true;
+                label1.Visible = true;
+                label2.Visible = true;
+                ConnectUser.Visible = false;
+            }
+        }
+        //register web 
+        private void registration_Click(object sender, EventArgs e)
+        {           
+            System.Diagnostics.Process.Start("http://google.com");
+
+        }
+        //off
+        private void Launcher_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (th != null) th.Abort();
+            Application.Exit();
+        }
+        // start game 
+        private void game_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("game.exe",msgserv.Substring(5));
+            if (th != null) th.Abort();
+            Application.Exit();
+        }
+    }
+}
